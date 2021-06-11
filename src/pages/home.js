e214ed40332e33d6cbdc699b4057bf00984e29eb
kimhyeonu <polaris@byeolmuri.com>
2021-06-11 15:33:04 +0900

    Button 컴포넌트 구현

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Button from '../components/Button';
+
 const Home = () => {
   return (
     <div>
       <h1>노터스</h1>
       <p>이곳은 홈 페이지입니다.</p>
+      <Button>클릭!</Button>
     </div>
   );
 };
