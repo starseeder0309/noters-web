f6860edbe23d66b11bb8379f3ffe4e8643843ebb
kimhyeonu <polaris@byeolmuri.com>
2021-06-11 15:39:36 +0900

    글로벌 스타일 컴포넌트 구현 및 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import GlobalStyle from './styles/GlobalStyle';
+
 import Pages from './pages';
 
 const App = () => {
   return (
     <div>
+      <GlobalStyle />
       <Pages />
     </div>
   );
