a8dc4c2d33b511eb1cb3acb2d80f04e5fc375848
kimhyeonu <polaris@byeolmuri.com>
2021-06-14 11:51:03 +0900

    홈 페이지 내 노트 피드 페이지네이션 구현

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 
+import Button from '../components/Button';
 import NoteFeed from '../components/NoteFeed';
 
 const GET_NOTES = gql`
@@ -34,7 +35,37 @@ const HomePage = () => {
     return <p>오류가 발생했습니다!</p>;
   }
 
-  return <NoteFeed notes={data.noteFeed.notes} />;
+  return (
+    <React.Fragment>
+      <NoteFeed notes={data.noteFeed.notes} />
+      {data.noteFeed.hasNextPage && (
+        <Button
+          onClick={() =>
+            fetchMore({
+              variables: {
+                cursor: data.noteFeed.cursor,
+              },
+              updateQuery: (previousResult, { fetchMoreResult }) => {
+                return {
+                  noteFeed: {
+                    cursor: fetchMoreResult.noteFeed.cursor,
+                    hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+                    notes: [
+                      ...previousResult.noteFeed.notes,
+                      ...fetchMoreResult.noteFeed.notes,
+                    ],
+                    __typename: 'noteFeed',
+                  },
+                };
+              },
+            })
+          }
+        >
+          더 불러오기
+        </Button>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default HomePage;
