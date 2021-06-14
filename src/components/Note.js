7230dd291499bf2af3e098aa98275a11c56bbb63
kimhyeonu <polaris@byeolmuri.com>
2021-06-14 11:19:10 +0900

    Note, NoteFeed 스타일링

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,17 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
+import { format } from 'date-fns';
+
+const StyledNote = styled.article`
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+const MetaData = styled.div`
+  @media (min-width: 500px) {
+    display: flex;
+    align-items: top;
+  }
+`;
+
+const UserActions = styled.div`
+  margin-left: auto;
+`;
+
+const MetaInfo = styled.div`
+  padding-right: 1em;
+`;
 
 const Note = ({ note }) => {
   return (
-    <article>
-      <img
-        src={note.author.avatar}
-        alt={`${note.author.username} 아바타`}
-        height="50px"
-      />{' '}
-      {note.author.username} {note.createdAt} {note.favoriteCount}{' '}
+    <StyledNote>
+      <MetaData>
+        <MetaInfo>
+          <img
+            src={note.author.avatar}
+            alt={`${note.author.username} 아바타`}
+            height="50px"
+          />
+        </MetaInfo>
+
+        <MetaInfo>
+          <em>by</em> {note.author.username} <br />
+          {format(note.createdAt, 'MMM Do YYYY')}
+        </MetaInfo>
+
+        <UserActions>
+          <em>즐겨찾기 수:</em> {note.favoriteCount}
+        </UserActions>
+      </MetaData>
+
       <ReactMarkdown source={note.content} />
-    </article>
+    </StyledNote>
   );
 };
 
